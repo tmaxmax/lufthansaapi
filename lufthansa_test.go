36dc0d7c97ccdd693d246924a75672dd42fef1ee
Teodor Maxim <tmaxmax@outlook.com>
2020-05-16 21:17:24 +0300

    Created Fetch Countries test

--- lufthansa_test.go
+++ lufthansa_test.go
@@ -1,16 +1,31 @@
 package lufthansa
 
 import (
+	"os"
 	"testing"
+
+	"github.com/tmaxmax/lufthansaapi/types"
 )
 
-type TestItem struct {
+type TestCountryURLItem struct {
 	f      CountriesParams
 	Result string
 }
 
+// userAPI is the format an API access information is written in my JSON file
+type userAPI struct {
+	APIName      string `json:"name"`
+	ClientID     string `json:"id"`
+	ClientSecret string `json:"secret"`
+}
+
+// MyAPIs is the format my accessible userAPIs credentials are passed
+type userAPIs struct {
+	APIs []API `json:"apis"`
+}
+
 func TestCountryURLs(t *testing.T) {
-	tests := []TestItem{
+	tests := []TestCountryURLItem{
 		{CountriesParams{}, ""},
 		{CountriesParams{Lang: "EN"}, "?lang=EN"},
 		{CountriesParams{CountryCode: "DK"}, "DK"},
@@ -20,6 +35,7 @@ func TestCountryURLs(t *testing.T) {
 		{CountriesParams{Lang: "EN", Limit: 20, Offset: 1}, "?lang=EN&limit=20&offset=1"},
 	}
 
+	t.Logf("Testing Country URLs...\n")
 	for i := range tests {
 		result := tests[i].f.ToURL()
 		if result != tests[i].Result {
@@ -27,5 +43,29 @@ func TestCountryURLs(t *testing.T) {
 		} else {
 			t.Logf("PASSED -- TEST %d", i)
 		}
+	}
+}
+
+func TestFetchCountries(t *testing.T) {
+	t.Logf("Testing Fetch Countries...\n")
+
+	api, err := NewAPI(os.Getenv("LOA_ID"), os.Getenv("LOA_SECRET"))
+	if err != nil {
+		t.Fatalf("API initialization failed: %v", err)
+	}
+	fetched, err := api.FetchCountries(CountriesParams{})
+	switch fetched.(type) {
+	case *types.CountriesResponse:
+		cr := fetched.(*types.CountriesResponse)
+		t.Logf("%+v", cr)
+	case *types.APIError:
+		cr := fetched.(*types.APIError)
+		t.Logf("%+v", cr)
+	case *types.TokenError:
+		cr := fetched.(*types.TokenError)
+		t.Logf("%+v", cr)
+	default:
+		cr := fetched.(string)
+		t.Errorf("%s", cr)
 	}
 }

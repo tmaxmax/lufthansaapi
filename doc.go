22f9d9f6e808c6cef68d3cfe3bf72cc8abb2eb80
Teodor Maxim <tmaxmax@outlook.com>
2020-05-16 13:25:07 +0300

    Changed link to Lufthansa Open API

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Package lufthansa makes working with the Lufthansa Open API (https://developer.lufthansa.com/docs/read/Home)
+// Package lufthansa makes working with the Lufthansa Open API (https://developer.lufthansa.com/docs)
 // much easier and convenient in Go. This package wraps the whole API functionality in different methods and
 // functions. The only thing you must have for this package to work is the client key and the client secret!
 package lufthansa

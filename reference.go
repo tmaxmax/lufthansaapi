34eda5570510d2b4ddfcbd03b24669e40954c51f
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2020-08-29 21:34:47 +0300

    Add generic iterator for all reference containers, Update RefParams ToURL
    As iterators are used for all references, duplicating the code wasn't a solution. This generic iterator makes the imlpementation of resource traversal DRY and easy to implement.
    Improve ToURL perfomance by removing Sprintf and also fall back to English if the language base confidence is 0

--- reference.go
+++ reference.go
@@ -1,8 +1,12 @@
 package lufthansa
 
 import (
-	"fmt"
+	"context"
+	"errors"
+	"io"
+	"strconv"
 	"strings"
+	"sync"
 
 	"golang.org/x/text/language"
 )
@@ -10,7 +14,53 @@ import (
 const (
 	mdsReferenceAPI = fetchAPI + "/mds-references"
 	referenceAPI    = fetchAPI + "/references"
+
+	metaVersion = "1.0.0"
 )
+
+var (
+	metaKeySelf     = metaKey{"self"}
+	metaKeyFirst    = metaKey{"first"}
+	metaKeyLast     = metaKey{"last"}
+	metaKeyNext     = metaKey{"next"}
+	metaKeyPrevious = metaKey{"previous"}
+	metaKeyRelated  = metaKey{"related"}
+
+	ErrInvalidMeta    = errors.New("lufthansa: reference: new meta version detected")
+	errMissingMetaKey = errors.New("lufthansa: iterator: fetchFromMeta: missing meta key")
+)
+
+type referenceAPIResponse interface {
+	apiResponse
+	metadata() *meta
+}
+
+type metaKey struct {
+	string
+}
+
+func (mk *metaKey) UnmarshalText(data []byte) error {
+	mk.string = string(data)
+	return nil
+}
+
+func (mk *metaKey) opposite() metaKey {
+	switch *mk {
+	case metaKeySelf:
+		return metaKeySelf
+	case metaKeyFirst:
+		return metaKeyLast
+	case metaKeyLast:
+		return metaKeyFirst
+	case metaKeyNext:
+		return metaKeyPrevious
+	case metaKeyPrevious:
+		return metaKeyNext
+	case metaKeyRelated:
+		return metaKeyRelated
+	}
+	return metaKey{}
+}
 
 type (
 	referenceNames         map[language.Tag]string
@@ -27,7 +77,7 @@ func (rn *referenceNames) make(arr []referenceNameUnmarshal) {
 	}
 }
 
-func (rn *referenceNames) copy() referenceNames {
+func (rn *referenceNames) Copy() referenceNames {
 	r := make(referenceNames)
 	for k, v := range *rn {
 		r[k] = v
@@ -37,24 +87,24 @@ func (rn *referenceNames) copy() referenceNames {
 
 // reference meta types
 type (
-	metaLinks         map[string]string
-	metaLinkUnmarshal struct {
-		Rel  string `xml:"Rel,attr" json:"@Rel"`
-		Href string `xml:"Href,attr" json:"@Href"`
+	metaLinks         map[metaKey]string
+	MetaLinkUnmarshal struct {
+		Rel  metaKey `xml:"Rel,attr" json:"@Rel"`
+		Href string  `xml:"Href,attr" json:"@Href"`
 	}
 	meta struct {
-		Version    string
-		Links      metaLinks
-		TotalCount int
+		version    string
+		links      metaLinks
+		totalCount int
 	}
 	metaUnmarshal struct {
 		Version    string              `xml:"Version,attr" json:"@Version"`
-		Links      []metaLinkUnmarshal `xml:"Link" json:"Link"`
+		Links      []MetaLinkUnmarshal `xml:"Link" json:"Link"`
 		TotalCount int                 `xml:"TotalCount" json:"TotalCount"`
 	}
 )
 
-func (ml *metaLinks) make(arr []metaLinkUnmarshal) {
+func (ml *metaLinks) make(arr []MetaLinkUnmarshal) {
 	*ml = make(metaLinks)
 	for i := range arr {
 		(*ml)[arr[i].Rel] = arr[i].Href
@@ -69,17 +119,173 @@ func (ml *metaLinks) copy() metaLinks {
 	return r
 }
 
+// checkVersion panics if the meta version isn't the same as the version this API implementation is based on
+func (m *meta) checkVersion() bool {
+	return m.version == metaVersion
+}
+
 func (m *meta) make(mu *metaUnmarshal) {
-	m.TotalCount = mu.TotalCount
-	m.Version = mu.Version
-	m.Links.make(mu.Links)
+	m.totalCount = mu.TotalCount
+	m.version = mu.Version
+	m.links.make(mu.Links)
 }
 
 func (m *meta) copy() meta {
 	return meta{
-		Version:    m.Version,
-		Links:      m.Links.copy(),
-		TotalCount: m.TotalCount,
+		version:    m.version,
+		links:      m.links.copy(),
+		totalCount: m.totalCount,
+	}
+}
+
+func (m *meta) hasSelf() bool {
+	_, ok := m.links[metaKeySelf]
+	return ok && m.checkVersion()
+}
+
+func (m *meta) fetch(a *API, ctx context.Context, rel metaKey) (bool, io.ReadCloser, error) {
+	if !m.checkVersion() {
+		return false, nil, ErrInvalidMeta
+	}
+	url, ok := m.links[rel]
+	if !ok {
+		return false, nil, nil
+	}
+	fetched, err := a.fetch(ctx, url)
+	if err != nil {
+		return false, nil, err
+	}
+	return true, fetched, nil
+}
+
+type iterator struct {
+	ref       referenceAPIResponse
+	api       *API
+	err       error
+	mu        sync.RWMutex
+	firstNext sync.Once
+}
+
+func (i *iterator) fetchFromMeta(ctx context.Context, rel metaKey) error {
+	ok, fetched, err := i.ref.metadata().fetch(i.api, ctx, rel)
+	if !ok {
+		if err != nil {
+			return err
+		}
+		return errMissingMetaKey
+	}
+	return i.ref.decode(fetched)
+}
+
+func (i *iterator) iterate(ctx context.Context, rel metaKey) bool {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	i.err = i.fetchFromMeta(ctx, rel)
+	if i.err == errMissingMetaKey {
+		if rel == metaKeyNext || rel == metaKeyPrevious {
+			*i = iterator{
+				ref: i.ref,
+				api: i.api,
+			}
+			i.mu.Lock()
+			m := i.ref.metadata()
+			*m = meta{
+				version: i.ref.metadata().version,
+				links: metaLinks{
+					metaKeyFirst:   m.links[metaKeyFirst],
+					rel.opposite(): m.links[metaKeySelf],
+					metaKeyLast:    m.links[metaKeyLast],
+				},
+				totalCount: m.totalCount,
+			}
+		}
+		return false
+	}
+	if i.err != nil {
+		return false
+	}
+	return true
+}
+
+// Next fetches the next set of countries, overwriting the current one. If an error occurs, the receiver is not overwritten.
+// The method whether further iterations can be done.
+// If you want to keep each set individually, call Countries.Copy after iterating to copy the newly fetched set.
+func (i *iterator) Next(ctx context.Context) bool {
+	iterated := false
+	i.firstNext.Do(func() {
+		i.mu.Lock()
+		defer i.mu.Unlock()
+
+		fetched, err := i.api.fetch(ctx, i.ref.metadata().links[metaKeyNext])
+		if err != nil {
+			i.err = err
+			return
+		}
+		if err = i.ref.decode(fetched); err != nil {
+			i.err = err
+			return
+		}
+		iterated = true
+	})
+	if !iterated && i.err == nil {
+		iterated = i.iterate(ctx, metaKeyNext)
+	}
+	return iterated
+}
+
+// Previous fetches the previous set of countries, overwriting the current one. If an error occurs, the receiver is not overwritten.
+// The method returns the receiver, which is nil when there is no preceding set available, or returns nil if an error occurred.
+// If you want to keep each set individually, call Countries.Copy after iterating to copy the newly fetched set.
+func (i *iterator) Previous(ctx context.Context) bool {
+	return i.iterate(ctx, metaKeyPrevious)
+}
+
+// HasSelf checks if the set can refetch itself from the API. This returns false only if the struct is in a state where
+// only Countries.Next and Countries.Previous are valid operations (the struct holds no data). Use this to check if the
+// data is available.
+func (i *iterator) HasSelf() bool {
+	i.mu.RLock()
+	defer i.mu.RUnlock()
+
+	return i.ref.metadata().hasSelf()
+}
+
+// Self refetches the last countries resource fetched, overwriting the current set.
+// If you want a copy of the current Countries struct, use the Countries.Copy method instead.
+func (i *iterator) Self(ctx context.Context) {
+	i.iterate(ctx, metaKeySelf)
+}
+
+// First fetches the first set of countries, overwriting the current one, if available.
+func (i *iterator) First(ctx context.Context) {
+	i.iterate(ctx, metaKeySelf)
+}
+
+// Last fetches the last set of countries, overwriting the current one, if available.
+func (i *iterator) Last(ctx context.Context) {
+	i.iterate(ctx, metaKeyLast)
+}
+
+// Error returns any errors that occurred while calling Countries.Next. It is idempotent, multiple calls after a single
+// iteration return the same result.
+func (i *iterator) Error() error {
+	i.mu.RLock()
+	defer i.mu.RUnlock()
+
+	return i.err
+}
+
+func (i *iterator) copy(newAPI *API) iterator {
+	i.mu.RLock()
+	defer i.mu.RUnlock()
+
+	if newAPI == nil {
+		newAPI = i.api
+	}
+	return iterator{
+		ref: i.ref,
+		api: newAPI,
 	}
 }
 
@@ -101,24 +307,44 @@ type RefParams struct {
 	Offset int
 }
 
+func writeAmp(sb *strings.Builder, mustWrite *bool) {
+	if *mustWrite {
+		sb.WriteByte('&')
+	} else {
+		sb.WriteByte('?')
+		*mustWrite = true
+	}
+}
+
 // ToURL transforms a CountriesParams struct into an URL usable format,
 // so that it can be concatenated to te Request API URL.
 func (p *RefParams) ToURL() string {
 	if p == nil {
 		return ""
 	}
-	var ret strings.Builder
+	mustWriteAmp := false
+	var sb strings.Builder
 	if p.code != "" {
-		ret.WriteString(p.code)
+		sb.WriteString(p.code)
 	}
 	if p.Lang != nil {
-		b, _ := p.Lang.Base()
-		ret.WriteString(fmt.Sprintf("?lang=%s", b.String()))
+		b, c := p.Lang.Base()
+		if c == language.No {
+			b, _ = language.English.Base()
+		}
+		sb.WriteString("?lang=")
+		mustWriteAmp = true
+		sb.WriteString(b.String())
 	}
-	if lo := processLimitOffset(p.Limit, p.Offset); lo != "" && strings.Contains(ret.String(), "?") {
-		ret.WriteString(fmt.Sprintf("&%s", lo))
-	} else if lo != "" {
-		ret.WriteString(fmt.Sprintf("?%s", lo))
+	if p.Limit != 0 {
+		writeAmp(&sb, &mustWriteAmp)
+		sb.WriteString("limit=")
+		sb.WriteString(strconv.Itoa(p.Limit))
 	}
-	return ret.String()
+	if p.Offset != 0 {
+		writeAmp(&sb, &mustWriteAmp)
+		sb.WriteString("offset=")
+		sb.WriteString(strconv.Itoa(p.Offset))
+	}
+	return sb.String()
 }
